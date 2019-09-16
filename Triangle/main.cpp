d84c3cbcc59229cfa2473f13edc16c6ee54a1cef
evopen <520dhh@gmail.com>
2019-09-16 18:56:00 +0800

    construct Vulkan app framework

---
+++ Triangle/main.cpp
@@ -0,0 +1,43 @@
+#include <vulkan/vulkan.h>
+#include <exception>
+#include <iostream>
+
+class HelloTriangleApplication
+{
+public:
+	void run()
+	{
+		initVulkan();
+		mainLoop();
+		cleanup();
+	}
+
+private:
+	void initVulkan()
+	{
+	}
+
+	void mainLoop()
+	{
+	}
+
+	void cleanup()
+	{
+	}
+};
+
+int main(int argc, char* argv[])
+{
+	HelloTriangleApplication app;
+
+	try
+	{
+		app.run();
+	}
+	catch (const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+		return EXIT_FAILURE;
+	}
+	return EXIT_SUCCESS;
+}
